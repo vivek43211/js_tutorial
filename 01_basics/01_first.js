c580c0b0b8284a7710a931b05ebeceb14dc01a92
vivek43211 <125999079+vivek43211@users.noreply.github.com>
2024-06-11 14:47:12 +0000

    basics data

---
+++ 01_basics/01_first.js
@@ -0,0 +1,10 @@
+console.log("hello worl!!!!")
+let accountId = 1234;
+var accountpassword = "33221@"
+const accountemail = "vyas@333"
+accountcity = "jaipur"
+console.log(accountId);
+accountId = 4321;
+accountpassword = "@23232"
+accountcity = "delhi"
+console.table([accountId,accountemail,accountpassword,accountcity])
