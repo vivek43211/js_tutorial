f40632b4ef1456418acf8850907c37683714c62d
vivek43211 <125999079+vivek43211@users.noreply.github.com>
2024-06-11 16:23:27 +0000

    typeof

---
+++ 01_basics/02_data.js
@@ -0,0 +1,17 @@
+// let sc = "33aa";
+// console.log(typeof sc);
+// console.log(typeof(sc))
+ 
+// let valueInNumber = Number(sc)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33" =33
+// "33a" =Nan
+//true = 1 , false = 0
+
+let name = "harry"
+let booleanIn = Boolean(name)
+console.log(booleanIn)
+
+//"harry" = true , "" = false
