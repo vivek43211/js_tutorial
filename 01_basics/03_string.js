c2bc99526ed4793ac3c558d1848830dfe72b0fc8
vivek43211 <125999079+vivek43211@users.noreply.github.com>
2024-06-12 01:55:29 +0000

    string to number

---
+++ 01_basics/03_string.js
@@ -0,0 +1,45 @@
+// let value = 3;
+// let negvalue = -value;
+// console.log(negvalue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2%2)
+// console.log(2/2)
+// console.log(2**3)
+// console.log(2*2)
+// let str1 = "hello"
+// let str2 = " world!!"
+// let str3 = str1 +str2
+// console.log(str3)
+
+//string to number
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"1")
+
+
+// comparsion of datatype
+// console.log(2>1)
+// console.log(2 >= 2)
+// console.log(2 < 1)
+// console.log(2 <= 1)
+// console.log(2 == 1)
+
+// console.log("2" == 1)
+// console.log("2" > 1)
+// console.log("2" < 1)
+
+// //=== it check both data type
+// console.log("2" === 1)
+
+console.log(null >0)
+console.log(null == 0 )
+console.log(null >=0)
+
+console.log(undefined >= 0)
+console.log(undefined == 0)
+console.log(undefined > 0)
+
+
