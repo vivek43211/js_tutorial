8f77951eff72880841d31bece6fe79daec46ac6c
vivek43211 <125999079+vivek43211@users.noreply.github.com>
2024-06-12 06:04:48 +0000

    date and time

---
+++ 01_basics/06_dateandtime.js
@@ -0,0 +1,20 @@
+let mydate = new Date();
+// console.log(mydate);
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleDateString());
+// console.log(mydate.toISOString());
+// console.log(typeof mydate);
+
+
+// let mycreatedate = new Date(2024,3,21)
+let mycreatedate = new Date(2024,5,12,5,7)
+// console.log(mycreatedate.toLocaleDateString())
+// console.log(mycreatedate.toLocaleString());
+
+let timestamp = Date.now()
+// console.log(timestamp);
+// console.log(mycreatedate.getTime());
+console.log(Math.floor(Date.now()/1000));
+
+
