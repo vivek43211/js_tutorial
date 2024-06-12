c8c990afde34a0c889bea76e1889125efe3559b9
vivek43211 <125999079+vivek43211@users.noreply.github.com>
2024-06-12 02:17:50 +0000

    stack and heap

--- 01_basics/03_string.js
+++ 01_basics/03_string.js
@@ -34,12 +34,33 @@
 // //=== it check both data type
 // console.log("2" === 1)
 
-console.log(null >0)
-console.log(null == 0 )
-console.log(null >=0)
+// console.log(null >0)
+// console.log(null == 0 )
+// console.log(null >=0)
 
-console.log(undefined >= 0)
-console.log(undefined == 0)
-console.log(undefined > 0)
+// console.log(undefined >= 0)
+// console.log(undefined == 0)
+// console.log(undefined > 0)
 
 
+
+/// memorys
+// stack(prmitive)= copy value
+//heap(non-primtive)= only you got references
+let myname = "userrrrr"
+let mynamecorrect = myname
+
+mynamecorrect = "user"
+console.log(mynamecorrect)
+console.log(myname)
+
+
+let obj = {
+    email : "user@email.com",
+    city : "jaipur"
+}
+
+let obj2 = obj;
+obj2.email="vivek@gamil.com"
+console.log(obj2)
+console.log(obj)
